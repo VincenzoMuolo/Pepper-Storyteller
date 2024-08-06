ce01d5ab8048296c4c2aec3f18bdb3153b4b0481
Vincenzo Muolo <56165423+VincenzoMuolo@users.noreply.github.com>
2024-08-06 15:50:12 +0200

    Update user_stories.php

--- user_stories.php
+++ user_stories.php
@@ -4,7 +4,11 @@ session_start();
 
 require_once('connection.php');
 
-$mySelectStatement = $conn->prepare("SELECT * FROM `Storie` WHERE Username = :username");
+$mySelectStatement = $conn->prepare("SELECT storie.*, tipo.Tipo 
+    FROM `Storie` storie
+    JOIN `TipoPubblicazione` tipo ON storie.Titolo = tipo.Storia
+    WHERE storie.Username = :username
+");
 $mySelectStatement->bindParam(':username', $_SESSION["Username"]);
 $mySelectStatement->execute();
 $userStoriesResult = $mySelectStatement->fetchAll(PDO::FETCH_ASSOC);
